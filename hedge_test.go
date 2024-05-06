871d875b27d9f46ac0a81b7d6ce371bbd557c61d
Annirudh Prasad <annirudh@wandb.com>
2024-05-06 14:52:16 -0700

    fix

--- hedge_test.go
+++ hedge_test.go
@@ -14,27 +14,27 @@ import (
 func TestHedgedRequestBasic(t *testing.T) {
 	ctx := context.Background()
 	count := 0
-	expectedResult := "success"
+	expected := "success"
 	requester := func(ctx context.Context) (string, error) {
 		count++
 		if count == 1 {
-			return expectedResult, nil
+			return expected, nil
 		} else {
 			return "fail", fmt.Errorf("should not trigger hedged request")
 		}
 	}
 
-	result, err := HedgedRequest[string](ctx, requester)
+	actual, err := HedgedRequest[string](ctx, requester)
 
 	require.NoError(t, err)
-	assert.Equal(t, expectedResult, result)
+	assert.Equal(t, expected, actual)
 }
 
 func TestHedgedRequestHedgingTriggered(t *testing.T) {
 	ctx := context.Background()
 	count := 0
 	delay := 50 * time.Millisecond
-	expectedResult := "success"
+	expected := "success"
 	requester := func(ctx context.Context) (string, error) {
 		count++
 		if count == 0 {
@@ -45,26 +45,59 @@ func TestHedgedRequestHedgingTriggered(t *testing.T) {
 				return "fail", ctx.Err()
 			}
 		} else {
-			return expectedResult, nil
+			return expected, nil
 		}
 	}
 
-	result, err := HedgedRequest[string](ctx, requester, WithDelay(delay), WithNumHedgedRequests(1))
+	actual, err := HedgedRequest[string](ctx, requester, WithDelay(delay), WithNumHedgedRequests(1))
 
 	require.NoError(t, err)
-	assert.Equal(t, expectedResult, result)
+	assert.Equal(t, expected, actual)
+}
+
+func TestHedgedRequestMultipleSuccess(t *testing.T) {
+	ctx := context.Background()
+	expected := "success"
+	delay := 5 * time.Millisecond
+	done := make(chan struct{})
+	requester := func(ctx context.Context) (string, error) {
+		// Synchronize on the done channel. The original request and
+		// all hedged requests will line up and block here.
+		select {
+		case <-done:
+			return expected, nil
+		case <-ctx.Done():
+			return "fail", ctx.Err()
+		}
+	}
+
+	// Wait for 2x the delay to ensure that all hedged requests have
+	// fired alongside the original request. Then close the done channel
+	// so the original and hedged requests complete almost simultaneously.
+	time.AfterFunc(2*delay, func() {
+		close(done)
+	})
+
+	actual, err := HedgedRequest[string](
+		ctx,
+		requester,
+		WithDelay(delay),
+	)
+
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual)
 }
 
 func TestHedgedRequestErrorPropagation(t *testing.T) {
 	ctx := context.Background()
-	expectedError := errors.New("failure")
+	expectedErr := errors.New("failure")
 	requester := func(ctx context.Context) (string, error) {
-		return "fail", expectedError
+		return "fail", expectedErr
 	}
 
 	_, err := HedgedRequest[string](ctx, requester)
 
-	require.ErrorIs(t, err, expectedError)
+	require.ErrorIs(t, err, expectedErr)
 }
 
 func TestHedgedRequestCancellation(t *testing.T) {
